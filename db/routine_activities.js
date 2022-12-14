364c82fc6481d0d8e08748a08cc1cac55b34f517
Nicholas Moore <nicholasjosephmoore@gmail.com>
2022-12-13 20:55:00 -0600

    add activity to routine

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,16 +1,34 @@
 const client = require("./client");
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    console.log("Got routines!")
 
-async function addActivityToRoutine({
-  routineId,
-  activityId,
-  count,
-  duration,
-}) {}
+    await client.query(`
+    
+    `)
+
+  }catch (error) {
+    console.log("error getting activites");
+    throw error;
+  }
+  return id
+}
+
+async function addActivityToRoutine({routineId, activityId, count, duration, sets}) {
+
+    const { rows } = await client.query(`
+      INSERT INTO routine_activities (routineId, activityId, count, duration, sets)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `,
+     [routineId, activityId, count, duration, sets]
+    );
+    return rows
+}
 
 async function getRoutineActivitiesByRoutine({ id }) {}
-console.log("getroutines" getRoutineActivitiesByRoutine)
+
 
 async function updateRoutineActivity({ id, ...fields }) {}
 
@@ -19,6 +37,7 @@ async function destroyRoutineActivity(id) {}
 async function canEditRoutineActivity(routineActivityId, userId) {}
 
 module.exports = {
+  client,
   getRoutineActivityById,
   addActivityToRoutine,
   getRoutineActivitiesByRoutine,
