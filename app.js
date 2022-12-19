6d7e63daa4c7378c473b81f60b1aeceef84dca38
Pierce Babineaux <propurtyschannel@gmail.com>
2022-12-19 00:30:54 -0600

    feat(api): add cors to server

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
 
 // Setup your Middleware and API Router here
 
