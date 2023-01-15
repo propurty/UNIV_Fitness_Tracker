fc6b466ab586d708e547d3c59b17296fde1373e4
Pierce Babineaux <propurtyschannel@gmail.com>
2023-01-14 21:13:24 -0600

    fix(server): fix server.js to connect client

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const http = require("http")
-const chalk = require("chalk")
-const app = require("./app")
+const http = require("http");
+const chalk = require("chalk");
+const app = require("./app");
+const { client } = require("./db");
 
-const PORT = process.env["PORT"] ?? 3000
-const server = http.createServer(app)
+const PORT = process.env["PORT"] ?? 3000;
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
+  client.connect();
   console.log(
     chalk.blueBright("Server is listening on PORT:"),
     chalk.yellow(PORT),
     chalk.blueBright("Get your routine on!")
-  )
-})
+  );
+});
